52d3bda8c1fbb0a897c00cec976bf16645e8e2aa
lcm <lcm8728@gmail.com>
2022-01-15 04:09:58 +0900

    Time: 1 ms (52.12%), Space: 40.4 MB (22.81%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,9 +1,10 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int n = nums.length;
-        int i = n - 1;
+        int i = n - 2;
         for(; i >= 0; --i) {
-            if(switchWithNextBigger(nums, i)) {
+            if(nums[i] < nums[i + 1]) {
+                switchWithNextBigger(nums, i);
                 break;
             }
         }
@@ -11,15 +12,13 @@ class Solution {
         reverse(nums, i + 1, n - 1);
     }
     
-    private boolean switchWithNextBigger(int[] nums, int current) {
+    private void switchWithNextBigger(int[] nums, int current) {
         for(int i = nums.length - 1; i > current; --i) {
             if(nums[current] < nums[i]) {
                 swap(nums, current, i);
-                return true;
+                return;
             }
         }
-        
-        return false;
     }
     
     private void reverse(int[] nums, int from, int to) {
