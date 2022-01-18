d0fffaf7753648ebf63c3826e00f5dccf12a4644
lcm <lcm8728@gmail.com>
2022-01-18 21:57:14 +0900

    Time: 0 ms (100.00%), Space: 39.6 MB (15.04%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -2,18 +2,13 @@ class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
         
-        for(int i = 0; i < n - 1; ++i) {
-            for(int j = 0; j < (n - 1) - i ; ++j) {
-                int dist = (n - 1) - i - j;
-                swap(matrix, i, j, i + dist, j + dist);
-            }
-        }
-        
-        for(int i = 0; i < n / 2; ++i) {
-            for(int j = 0; j < n ; ++j) {
-                swap(matrix, i, j, n - 1 - i, j);
-            }
-        }
+        for(int i = 0; i < n - 1; ++i)
+            for(int j = 1 + i; j < n ; ++j)
+                swap(matrix, i, j, j, i);
+    
+        for(int i = 0; i < n; ++i)
+            for(int j = 0; j < n / 2 ; ++j)
+                swap(matrix, i, j, i, n - 1 - j);
     }
     
     private void swap(int[][] matrix, int r1, int c1, int r2, int c2) {
